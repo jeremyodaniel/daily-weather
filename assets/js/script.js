ca90a931134d900ba4a455e5030efabac44f7dc5
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-05-12 00:16:09 -0500

    Add functionality to the city search form to search any city.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
-//var cityFormEl = document.querySelector("#city-form");
-//var cityInputEl = document.querySelector("#city");
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
 
 
 
 
 
-// var formSubmitHandler = function(event) {
-//   event.preventDefault();
-// // get value from input element
-// var city = cityInputEl.value.trim();
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+	// get value from input element
+	var city = cityInputEl.value.trim();
+	
+	if (city) {
+		getWeatherData(city);
+		cityInputEl.value = "";
+	} else {
+		alert("Please enter a valid city");
+	}
+};
 
 var getWeatherData = function(city) {
 	// ADD PARAMS FOR TEMP, HUMIDITY, WIND SPEED, & UV INDEX
@@ -23,9 +31,4 @@ var getWeatherData = function(city) {
 };
 
 
-getWeatherData("portland");
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
-//71fbb881f4c77b12293418473320c833
-
-// DOM manipulation
+cityFormEl.addEventListener("submit", formSubmitHandler);
